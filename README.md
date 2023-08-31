686186ff7fa00696b52ae3150d069ed4ba5dbfeb
an <habinhan1999@gmail.com>
2023-08-31 17:49:57 +0700

    download firecracker

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # firecracker-test
+
+```
+wget https://raw.githubusercontent.com/bannnn511/firecracker-test/main/install-firecracker.sh
+&& 
+chmod +x install-firecracker.sh 
+&& 
+./install-firecracker.sh
+```
--- install-firecracker.sh
+++ install-firecracker.sh
@@ -1,3 +1,13 @@
+release_url="https://github.com/firecracker-microvm/firecracker/releases"
+latest=$(basename $(curl -fsSLI -o /dev/null -w %{url_effective} ${release_url}/latest))
+arch=$(uname -m)
+curl -L ${release_url}/download/${latest}/firecracker-${latest}-${arch}.tgz |
+    tar -xz
+
+mv firecracker-${latest}-$(uname -m) firecracker
+
+cd firecracker
+
 arch=$(uname -m)
 dest_kernel="hello-vmlinux.bin"
 dest_rootfs="hello-rootfs.ext4"
